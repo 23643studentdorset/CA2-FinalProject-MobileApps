cb56bbad12754954d15ac264c13c67012cb296c2
23643@student.dorset-college.ie <23643@student.dorset-college.ie>
2022-12-17 01:05:53 +0000

    .

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
+  useState(() => { 
+    const getPlaces = async () => {
+      try {
+        const response = await fetch('https://gist.githubusercontent.com/saravanabalagi/541a511eb71c366e0bf3eecbee2dab0a/raw/bb1529d2e5b71fd06760cb030d6e15d6d56c34b3/places.json');
+        const places = await response.json();
+        console.log(places);
+      } 
+      catch (error) {
+        console.error(error);
+      } 
+    }
+    getPlaces()
+
+  }, []); 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    {/*Render our MapView*/}
+      <MapView
+        style={styles.map}
+        //specify our coordinates.
+        initialRegion={{
+          latitude: 53.4494762,
+          longitude: -7.8529786,
+          latitudeDelta:5.3,
+          longitudeDelta: 5.3,
+        }}
+      />
     </View>
   );
 }
-
+//create our styling code:
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    ...StyleSheet.absoluteFillObject,
+    flex: 1, //the container will fill the whole screen.
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
